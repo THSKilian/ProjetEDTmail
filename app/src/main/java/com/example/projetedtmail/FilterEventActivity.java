9f86492065e381e771056b103aa0ec56a48c4bd8
THSKilian <kilian.thomas24@gmail.com>
2021-03-02 16:19:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetedtmail/FilterEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetedtmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilterEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_event);
+    }
+}
